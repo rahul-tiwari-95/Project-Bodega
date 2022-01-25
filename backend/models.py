603f7cca72595e17b6c1dacb567d0dae06e73e69
rahul-tiwari-95 <knowrahul.95@gmail.com>
2022-01-25 14:13:20 -0500

    database connected

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=40)
+
+    
--- trill/settings.py
+++ trill/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'backend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'trill.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'trillmeta',
+        'USER': 'postgres',
+        'PASSWORD': '043074',
+        'HOST' : '127.0.0.1',
+        'PORT':'5432'
     }
 }
 
