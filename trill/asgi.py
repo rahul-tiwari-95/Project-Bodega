7a02192e9d8fe6e31b5ef8a7bdd1514b33d472f4
rahul-tiwari-95 <knowrahul.95@gmail.com>
2022-02-20 13:16:27 -0500

    setting up asgi.py

--- trill/asgi.py
+++ trill/asgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trill.settings')
+# If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
+# on Azure App Service and should use the production settings in production.py.
+settings_module = "azuresite.production" if 'WEBSITE_HOSTNAME' in os.environ else 'azuresite.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
--- trill/wsgi.py
+++ trill/wsgi.py
@@ -11,7 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = "trill.production" if 'WEBSITE_HOSTNAME' in os.environ else 'trill.settings'
+# If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
+# on Azure App Service and should use the production settings in production.py.
+settings_module = "azuresite.production" if 'WEBSITE_HOSTNAME' in os.environ else 'azuresite.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
