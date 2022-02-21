033e52b0877b8cc32f3a36be82287dde63341251
rahul-tiwari-95 <knowrahul.95@gmail.com>
2022-02-21 13:59:25 -0500

    CSRF settings

--- trill/settings.py
+++ trill/settings.py
@@ -16,6 +16,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CSRF_TRUSTED_ORIGINS =[
+    'https://*.bdgdao.azurewebsites.net'
+]
 #hello
 
 # Quick-start development settings - unsuitable for production
