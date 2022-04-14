4db9157fe4888d0129130c931cc451bc44cc130a
raven88 <42743946+rahul-tiwari-95@users.noreply.github.com>
2022-04-14 11:52:26 -0400

    CORS-POSTCsrf
    Added PostCSRF code to Settings.py --> Pushing to dev

--- trill/settings.py
+++ trill/settings.py
@@ -70,6 +70,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware'
+
 ]
 
 CORS_ORIGIN_ALLOW_ALL=True
